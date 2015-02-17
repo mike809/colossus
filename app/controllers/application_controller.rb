e74a3df64dfa4eff1f938a42b3bc90c677265378
Richard Garcia <richard.g@outlook.com>
2015-02-16 21:57:45 -0800

    configuring custom devise permitted params (username, email) to C_U_

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    registration_params = [:email, :password, :password_confirmation, :username, :name]
+
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(registration_params)
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(registration_params)
+    end
+    
+    devise_parameter_sanitizer.for(:sign_in) do |u|
+      u.permit(registration_params)
+    end
+  end
 end
